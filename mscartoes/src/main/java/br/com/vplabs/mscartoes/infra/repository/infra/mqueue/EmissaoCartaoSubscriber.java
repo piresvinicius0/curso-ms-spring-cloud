bee70338589c3501d0eb8411191a7e4b6c8cbd6c
Vinicius Vieira Pires <vinicius.pires@cwi.com.br>
2023-05-30 09:52:06 -0300

    adição do spring actuator

--- mscartoes/src/main/java/br/com/vplabs/mscartoes/infra/repository/infra/mqueue/EmissaoCartaoSubscriber.java
+++ mscartoes/src/main/java/br/com/vplabs/mscartoes/infra/repository/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -8,12 +8,14 @@ import br.com.vplabs.mscartoes.infra.repository.ClienteCartaoRepository;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class EmissaoCartaoSubscriber {
 
     private final CartaoRepository cartaoRepository;
@@ -31,7 +33,7 @@ public class EmissaoCartaoSubscriber {
             clienteCartao.setLimite(dados.getLimiteLiberado());
             clienteCartaoRepository.save(clienteCartao);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("Erro ao receber solicitação de emissão de cartão: {}", e.getMessage());
         }
     }
 }
