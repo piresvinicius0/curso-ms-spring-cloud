685303531a68f7104d7ebae496bd76d7a6751230
Vinicius Vieira Pires <vinicius.pires@cwi.com.br>
2023-06-21 22:18:55 -0300

    adiciona security

---
+++ eurekaserver/src/main/java/br/com/vplabs/eurekaserver/security/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package br.com.vplabs.eurekaserver.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
